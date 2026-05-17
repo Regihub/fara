013ffd8134ecaa3401430250e9db2f8e756fd8c1
Regihub <42317575+Regihub@users.noreply.github.com>
2026-05-17 23:42:00 +0200

    Update index.js

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,1 +1,16 @@
+export default function handler(req, res) {
+  const clientId = process.env.GITHUB_CLIENT_ID;
 
+  const redirectUri =
+    'https://fara.vercel.app/api/auth/callback';
+
+  const scope = 'repo';
+
+  const url =
+    `https://github.com/login/oauth/authorize` +
+    `?client_id=${clientId}` +
+    `&scope=${scope}` +
+    `&redirect_uri=${encodeURIComponent(redirectUri)}`;
+
+  res.redirect(url);
+}
