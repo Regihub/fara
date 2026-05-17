159c0214a7c17fc64a80f149386236647fee5409
Regihub <42317575+Regihub@users.noreply.github.com>
2026-05-17 23:47:06 +0200

    Implement GitHub OAuth callback handler

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -1,1 +1,45 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
 
+  const client_id = process.env.GITHUB_CLIENT_ID;
+  const client_secret = process.env.GITHUB_CLIENT_SECRET;
+
+  const tokenRes = await fetch(
+    'https://github.com/login/oauth/access_token',
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        client_id,
+        client_secret,
+        code,
+      }),
+    }
+  );
+
+  const data = await tokenRes.json();
+
+  if (data.error) {
+    return res.status(400).send(data.error_description || data.error);
+  }
+
+  const html = `
+<!doctype html>
+<html>
+  <body>
+    <script>
+      window.opener.postMessage(
+        'authorization:github:success:${data.access_token}',
+        '*'
+      );
+      window.close();
+    </script>
+  </body>
+</html>`;
+
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(html);
+}
