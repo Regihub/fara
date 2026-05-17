4ad23fd690ef7911c3c654aa4c8fba6556074c20
Regihub <42317575+Regihub@users.noreply.github.com>
2026-05-17 19:43:36 +0000

    Add Decap CMS OAuth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import decapCmsOauth from 'astro-decap-cms-oauth';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    decapCmsOauth()
+  ]
 });
